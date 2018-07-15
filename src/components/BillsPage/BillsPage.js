d74488b77b05918defd06bad9b22e07b7fd8e083
michael <michael.clark1992@gmail.com>
2018-07-14 22:39:48 -0500

    added page components

---
+++ src/components/BillsPage/BillsPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './BillsPage.css';
+
+class BillsPage extends React.Component {
+  render () {
+    return (
+      <div className="BillsPage">
+        <h1>Bills Page</h1>
+      </div>
+    );
+  }
+};
+
+export default BillsPage;
---
+++ src/components/IndividualBillPage/IndividualBillPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './IndividualBillPage';
+
+class IndividualBillPage extends React.Component {
+  render () {
+    return (
+      <div className="IndividualBillPage">
+        <h1>Individual Bill</h1>
+      </div>
+    );
+  }
+};
+
+export default IndividualBillPage;

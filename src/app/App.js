4e2831c97fccaa9f9ee4e6e9a71eb6adbf454c47
michael <michael.clark1992@gmail.com>
2018-07-14 22:50:24 -0500

    added routes

--- src/app/App.js
+++ src/app/App.js
@@ -1,19 +1,69 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import {Route, BrowserRouter, Redirect, Switch} from 'react-router-dom';
 import './App.css';
 
-class App extends Component {
+import Navbar from '../components/Navbar/Navbar';
+import Home from '../components/Home/Home';
+import BillsPage from '../components/BillsPage/BillsPage';
+import VotesPage from '../components/VotesPage/VotesPage';
+import IndividualBillPage from '../components/IndividualBillPage/IndividualBillPage';
+import Register from '../components/Register/Register';
+import Login from '../components/Login/Login';
+
+const PrivateRoute = ({component: Component, authed, ...rest}) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authed === true ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{pathname: '/login', state: {from: props.location}}}
+          />
+        )
+
+      }
+    />
+  );
+};
+const PublicRoute = ({ component: Component, authed, ...rest}) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authed === false ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: '/bills', state: {from: props.location}}}
+          />
+        )
+      }
+    />
+  );
+};
+
+class App extends React.Component {
+  state = {
+    authed: true,
+  }
   render () {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button className="btn btn-info">Bootstrapped</button>
+        <BrowserRouter>
+          <div>
+            <Navbar />
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <PublicRoute path="/login" authed={this.state.authed} component={Login} />
+              <PublicRoute path="/register" authed={this.state.authed} component={Register} />
+              <PrivateRoute path="/bills" authed={this.state.authed} component={BillsPage} />
+              <PrivateRoute path="/votes" authed={this.state.authed} component={VotesPage} />
+              <PrivateRoute path="/bill/:slug" authed={this.state.authed} component={IndividualBillPage} />
+            </Switch>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }

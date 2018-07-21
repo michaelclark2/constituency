8b23b20bf7c6fef01e1e71c755a1c9251516cac1
michael <michael.clark1992@gmail.com>
2018-07-21 01:45:57 -0500

    touched up individual bill page, added icons on navbar

--- src/components/IndividualBillPage/IndividualBillPage.js
+++ src/components/IndividualBillPage/IndividualBillPage.js
@@ -47,11 +47,9 @@ class IndividualBillPage extends React.Component {
         return (
           <div key={action.id} className="col-sm-8 col-sm-offset-2">
             <div className="panel panel-default">
-              <div className="panel-heading">
-                <h2 className="panel-title">
-                  <strong>{moment(action.datetime).format('LL')}</strong>
-                  <em> {action.chamber} {action.action_type}</em>
-                </h2>
+              <div className="panel-heading clearfix">
+                <h2 className="panel-title pull-left"><strong>{moment(action.datetime).format('LL')}</strong></h2>
+                <h2 className="panel-title pull-right"><em> {action.chamber} {action.action_type}</em></h2>
               </div>
               <div className="panel-body">
                 <p>{action.description}</p>
@@ -63,18 +61,19 @@ class IndividualBillPage extends React.Component {
     }
     return (
       <div className="IndividualBillPage container">
-        <div className="bill-title text-center col-xs-12">
-          <h1>{bill.number}</h1>
-          <h2>{bill.sponsor_title} {bill.sponsor_name || bill.sponsor}, {bill.sponsor_state} <em>with {bill.cosponsors} cosponsors</em></h2>
-          <h2>{bill.title}</h2>
+        <div className="bill-title text-center col-xs-12 clearfix">
+          <div className="clearfix">
+            <h1 className="bill-number pull-left">{bill.number}</h1>
+            <h3 className="pull-right">{bill.sponsor_title} {bill.sponsor_name || bill.sponsor}, {bill.sponsor_state} <em>with {bill.cosponsors} cosponsors</em></h3>
+          </div>
+          <div className="text-left">
+            <h4>{bill.committees}</h4>
+            <h2>{bill.title}</h2>
+          </div>
           <Ballot bill={bill} votes={votes} updateVotes={this.updateVotes}/>
         </div>
-        <div className="bill-body text-center col-xs-12">
-          <div className="col-xs-12">
-            <h3>Committees</h3>
-            <h4>{bill.committees}</h4>
-          </div>
-          <div className="col-xs-12">
+        <div className="bill-body text-center clearfix">
+          <div className="col-xs-12 summary">
             <h3>Summary</h3>
             {
               bill.summary ? (
@@ -86,13 +85,13 @@ class IndividualBillPage extends React.Component {
 
             <a className="btn btn-info" href={bill.congressdotgov_url + '/text'}>View Full Bill Text</a>
           </div>
-          <div className="col-xs-12">
+          <div className="col-xs-12 activity-container">
             <h3>Bill History</h3>
-            <div className="activity-container">
+            <div className="activities clearfix">
               {actionComponents}
             </div>
           </div>
-          <div className="col-xs-12">
+          <div className="col-xs-12 messages">
             <h3>Leave a Comment</h3>
             <MessageBoard bill={bill} votes={votes} />
           </div>

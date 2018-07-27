1bf41dd76058c2bc7b051b1b5e6a19d9193d4555
michael <michael.clark1992@gmail.com>
2018-07-26 21:47:32 -0500

    rerestyled homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import america from './img/america.svg';
 import './Home.css';
 
 class Home extends React.Component {
@@ -7,6 +8,7 @@ class Home extends React.Component {
       <div className="Home text-center clearfix">
         <div className="jumbotron">
           <div className="container">
+            <img src={america} alt="America" />
             <h1>Constituency</h1>
             <p>From the congressional floor to your fingertips</p>
             <ul>
